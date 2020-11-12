c14c40e63973c9db56eef964db1f14009d2a0650
majickdave <majickdave@gmail.com>
2020-11-12 15:57:34 -0500

    tested script on sanitized data

---
+++ forecast.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+from fbprophet import Prophet
+import pandas as pd
+
+h = pd.read_csv('holidays.csv')
+
+def plot_time_vol(df):
+    plt.subplot(221)
+    df['handle_time'].plot.box()
+
+    plt.subplot(222)
+    df['volume'].plot.box()
+
+    plt.subplot(223)
+    df['handle_time'].hist(grid=False)
+
+    plt.subplot(224)
+    df['volume'].hist(grid=False)
+    plt.tight_layout()
+    plt.show()
+
+
+def create_training_data(df, kpi, start_date, end_date):
+    # create a new dataframe with 2 columns, date and actual volume received
+    df['date'] = df.index
+    df = df[['date', kpi]]
+
+    # Must pre-format column names
+    df.columns = ['ds','y']
+
+    # set start date of training data
+    df = df[df['ds'] >= start_date]
+
+    # set end date of training data
+    df = df[df['ds'] < end_date]
+
+    return df
+
+def create_forecast(df,periods=200):
+    # create default prophet model
+    m = Prophet(weekly_seasonality=True, yearly_seasonality=True, daily_seasonality=False)
+    m.fit(df)
+
+    future = m.make_future_dataframe(periods=periods)
+    # display(future.tail())
+
+    # get rid of holidays and weekends
+    future = future[(~future['ds'].isin(h.iloc[:,0].tolist())) & (~future['ds'].dt.weekday.isin([5,6]))]
+
+    return m,future
+
+def plot_forecast(forecast):
+    # view only the last 5 predictions with confidence intervals 
+    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+    fig1 = m.plot(forecast)
+
+    fig2 = m.plot_components(forecast)
+
+def set_forecast(forecast, df1, start_date, end_date):
+    # set forecast beginning and end date
+    f = forecast.copy()
+    f = f[(forecast['ds'] >= start_date) & (f['ds'] <= end_date)]
+
+    # create validation dataset
+    df2 = df1.copy()
+    df2['ds'] = pd.to_datetime(df2.index.date) 
+
+    # set start date of validation data equal to June 1st, 2020
+    df2 = df2[df2['ds'] >= start_date]
+
+    # remove weekends and holidays
+    df2 = df2[~df2.index.isin(h.iloc[:,0].tolist())]
+    df2 = df2[~df2.index.weekday.isin([5,6])]
+    
+    return f, df2
+
+def validate_dates(f, df2):
+    """
+    take in forecast, f
+    and dataframe df2 and return Bool
+    """
+    res = []
+    # validate forecast dates shape equals actual dates shape
+    v1 = df2.shape[0] == f.shape[0]
+    if not v1:
+        return False
+    else: res.append(v1)
+        
+    # validate all dates for forecast equal actual dates
+    res.append(all(df2['ds'].dt.date.values == f['ds'].dt.date.values))
+    
+    return all(res)
+
+def evaluate_model(forecast, kpi, metric='mae'):
+    if metric == 'mae':
+        mae1 = mean_absolute_error(df2[kpi], df2[kpi+'_forecast'])
+        mae2 = mean_absolute_error(df2[kpi], f['yhat'])
+        diff = mae1- mae2
+        return {'old': mae1, 'prophet': mae2}
+    else:
+        return 'invalid metric'
---
+++ run_model.py
@@ -0,0 +1,174 @@
+
+from fbprophet import Prophet
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import mean_absolute_error
+import sys
+
+file_name, kpi, start_train, end_train = (sys.argv[1], 
+sys.argv[2], 
+sys.argv[3], 
+sys.argv[4])
+
+def plot_time_vol(df):
+    plt.subplot(221)
+    df['handle_time'].plot.box()
+
+    plt.subplot(222)
+    df['volume'].plot.box()
+
+    plt.subplot(223)
+    df['handle_time'].hist(grid=False)
+
+    plt.subplot(224)
+    df['volume'].hist(grid=False)
+    plt.tight_layout()
+    plt.show()
+
+
+def create_training_data(df, kpi, start_date, end_date):
+    # create a new dataframe with 2 columns, date and actual volume received
+    df['date'] = df.index
+    df = df[['date', kpi]]
+
+    # Must pre-format column names
+    df.columns = ['ds','y']
+
+    # set start date of training data
+    df = df[df['ds'] >= start_date]
+
+    # set end date of training data
+    df = df[df['ds'] < end_date]
+
+    return df
+
+def create_forecast(df,periods=200):
+    # create default prophet model
+    m = Prophet(weekly_seasonality=True, yearly_seasonality=True, daily_seasonality=False)
+    m.fit(df)
+
+    future = m.make_future_dataframe(periods=periods)
+    # display(future.tail())
+
+    # get rid of holidays and weekends
+    future = future[(~future['ds'].isin(h.iloc[:,0].tolist())) & (~future['ds'].dt.weekday.isin([5,6]))]
+
+    return m,future
+
+def plot_forecast(forecast):
+    # view only the last 5 predictions with confidence intervals 
+    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+    fig1 = m.plot(forecast)
+
+    fig2 = m.plot_components(forecast)
+
+def set_forecast(forecast, df1, start_date, end_date):
+    # set forecast beginning and end date
+    f = forecast.copy()
+    f = f[(forecast['ds'] >= start_date) & (f['ds'] <= end_date)]
+
+    # create validation dataset
+    df2 = df1.copy()
+    df2['ds'] = pd.to_datetime(df2.index.date) 
+
+    # set start date of validation data equal to June 1st, 2020
+    df2 = df2[df2['ds'] >= start_date]
+
+    # remove weekends and holidays
+    df2 = df2[~df2.index.isin(h.iloc[:,0].tolist())]
+    df2 = df2[~df2.index.weekday.isin([5,6])]
+    
+    return f, df2
+
+def validate_dates(f, df2):
+    """
+    take in forecast, f
+    and dataframe df2 and return Bool
+    """
+    res = []
+    # validate forecast dates shape equals actual dates shape
+    v1 = df2.shape[0] == f.shape[0]
+    if not v1:
+        return False
+    else: res.append(v1)
+        
+    # validate all dates for forecast equal actual dates
+    res.append(all(df2['ds'].dt.date.values == f['ds'].dt.date.values))
+    
+    return all(res)
+
+def evaluate_model(forecast, kpi, metric='mae'):
+    if metric == 'mae':
+        mae1 = mean_absolute_error(df2[kpi], df2[kpi+'_forecast'])
+        mae2 = mean_absolute_error(df2[kpi], f['yhat'])
+        diff = mae1- mae2
+        return {'old': mae1, 'prophet': mae2}
+    else:
+        return 'invalid metric'
+
+def validate_holidays(df):
+    # validate no weekends or holidays
+    return (df.index.weekday.isin([5,6]).any(),
+    df.index.isin(h.iloc[:,0].tolist()).any()) == (False, False)
+
+## Start script
+# Load Data
+
+
+df = pd.read_csv(file_name, parse_dates=['date'],
+                index_col=['date'], usecols=range(5), header=1,
+                names=['date', 'handle_time', 'handle_time_forecast', 
+                       'volume','volume_forecast'])
+df1 = df.copy()
+h = pd.read_csv('holidays.csv')
+
+# remove holidays
+df = df[~df.index.isin(h.iloc[:,0].tolist())]
+
+# remove weekends
+df = df[~df.index.weekday.isin([5,6])]
+# print(validate_holidays(df))
+
+# remove outliers
+df = df[df['handle_time'] > 10000]
+
+# plot data
+# plot_time_vol(df)
+
+# create training
+df = create_training_data(df, kpi, start_train, end_train)
+
+# plot forecast
+# display(df.head())
+m,future = create_forecast(df)
+forecast = m.predict(future)
+# plot_forecast(forecast)
+
+# set forecast beginning and end date
+f = forecast.copy()
+f = f[(forecast['ds'] >= end_train) & (f['ds'] <= df1.index.max())]
+
+# create validation dataset
+df2 = df1.copy()
+df2['ds'] = pd.to_datetime(df2.index.date) 
+
+# set start date of validation data equal to June 1st, 2020
+df2 = df2[df2['ds'] >= end_train]
+
+# remove weekends and holidays
+df2 = df2[~df2.index.isin(h.iloc[:,0].tolist())]
+df2 = df2[~df2.index.weekday.isin([5,6])]
+
+# Validate test data
+if validate_dates(f, df2):
+    mae = evaluate_model(f, kpi, metric='mae')
+    print(mae)
+#     display(pd.DataFrame(index=['old model', 'prophet', 'difference'], 
+#                  data=[int(mae1), int(mae2), int(mae1-mae2)],
+#                  columns=['MAE (volume)']))
+else:
+    print('invalid dates')
+
+print(forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']])
